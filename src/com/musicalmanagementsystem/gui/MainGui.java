fdd70d2feb1f891ace75c1e3724f54b7f593dedc
Sara <jsmbeaudet@gmail.com>
2018-12-09 16:47:11 -0500

    Actual GUI created, integrated with Main.java
    Here I added a GUI, a pane, some text. When the pane is clicked, it causes a text box to say "You just clicked!" and when clicked again, that text disappears.

--- src/com/musicalmanagementsystem/gui/MainGui.java
+++ src/com/musicalmanagementsystem/gui/MainGui.java
@@ -1,25 +1,59 @@
 package com.musicalmanagementsystem.gui;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class MainGui extends Application {
 
+
+	/*@FXML public TextField nameField;
+    @FXML public Button nameSubmitButton;
+	@FXML public Text helloBox;*/
+	@FXML public Pane addNewTaskPane;
+	@FXML public Text testingText;
+	boolean hasBeenClicked = false;
+	
 	public static void main(String[] args) {
 		launch(args);
+		
 	}
 	
 	public void start(Stage stage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("startPage.fxml"));
+		
+		FXMLLoader loader = new FXMLLoader(
+				getClass().getResource("startPage.fxml")
+		);
+		loader.setController(this);
+		Parent root = (Parent) loader.load();
 	    
 		Scene scene = new Scene(root, 1000, 800);
-	    
-		stage.setTitle("FXML Welcome");
+		
+		addNewTaskPane.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent e) {
+				if (hasBeenClicked) {
+					testingText.setText("");
+					hasBeenClicked = false;
+				} else {
+					testingText.setText("You just clicked!");
+					hasBeenClicked = true;
+				}
+			}
+		});
+		
+		stage.setTitle("Musical Management System");
 		stage.setScene(scene);
 		stage.show();
+		
+		
 	}
 
 }
